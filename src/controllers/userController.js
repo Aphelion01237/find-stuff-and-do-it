af6b247a406a702562b6764c494190bd374067d2
Aphelion <Aphelion01237@gmail.com>
2019-10-24 22:39:37 -0700

    working on user login routes

---
+++ src/controllers/userController.js
@@ -0,0 +1,18 @@
+const connection = require('./../../config/connection');
+
+module.exports = {
+        checkUser: (req, res) => {
+        const { usernameInput } = req.params;
+        const { passwordInput } = req.params;
+        const query = `SELECT * FROM users WHERE ?`;
+        connection.query(query, {username: usernameInput, password: passwordInput}, (err, events) => {
+            if(err) {
+                return res.status(404).send(err);
+            }
+            res.json(event);
+        });
+    }
+
+
+
+}
